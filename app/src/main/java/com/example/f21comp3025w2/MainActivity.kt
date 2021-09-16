56a65f0a17d40753229b08d139afc1275d0d8c83
JaretWright <jaret.wright@georgiancollege.ca>
2021-09-16 14:30:14 -0400

    Updated the controller to ask for the password

--- app/src/main/java/com/example/f21comp3025w2/MainActivity.kt
+++ app/src/main/java/com/example/f21comp3025w2/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.f21comp3025w2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.f21comp3025w2.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            if (binding.passwordEditText.text.toString().equals("12345"))
+            {
+                Snackbar.make(binding.loginLayout, "Welcome ${binding.nameEditText.text}",Snackbar.LENGTH_INDEFINITE).show()
+            }
+            else
+            {
+                Toast.makeText(this, "Invalid Login", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
